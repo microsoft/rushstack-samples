dd49cd4cc105dc60e37aff587a3f8e11d62a3299
Elliot Nelson <elliot-nelson@users.noreply.github.com>
2021-10-02 10:35:14 -0400

    specify 0.0.0.0

--- heft/heft-fastify-tutorial/src/server.ts
+++ heft/heft-fastify-tutorial/src/server.ts
@@ -9,6 +9,7 @@ import app from './app';
 //
 // For this example we'll keep it simple and avoid any argument processing.
 const port: number = Number(process.env.PORT) || 3000;
+const host: string = process.env.NODE_ENV === 'production' ? '0.0.0.0' : '127.0.0.1';
 const logfile: string = process.env.NODE_ENV === 'production' ? '/var/log/fastify.log' : 'fastify.log';
 
 const instance: FastifyInstance = fastify({
@@ -20,7 +21,7 @@ const instance: FastifyInstance = fastify({
 
 async function start(): Promise<void> {
   await instance.register(app);
-  await instance.listen(port);
+  await instance.listen(port, host);
 }
 
 start().catch((error) => {
